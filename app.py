452a7a430f5b9b774ace706c66be9df85a0c7b7f
hectorastrom <hector.astrom@gmail.com>
2021-09-03 14:53:36 -0400

    Added login and register bases

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+#from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt()
+# To hash do bycrypt.generate_password_hash(password).decode('utf-8')
+# To check password do bycrypt.check_password_hash(hashed_password, password)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
