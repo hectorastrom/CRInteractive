93f6dfd51833bb6d3a2d9cf08610aa7ea3c56536
hectorastrom <hector.astrom@gmail.com>
2021-09-03 15:18:31 -0400

    Added setup for databases

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from datetime import datetime
+
+from sqlalchemy.orm import backref
 
 
 
@@ -9,6 +12,33 @@ bcrypt = Bcrypt()
 # To hash do bycrypt.generate_password_hash(password).decode('utf-8')
 # To check password do bycrypt.check_password_hash(hashed_password, password)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    team = db.Column(db.String(20), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    twoks = db.relationship('Twok', backref='rower', lazy=True)
+
+    def __repr__():
+        return f"User(Firstname: '{self.firstname}', Lastname: '{self.lastname}', Email: '{self.email}', Team: '{self.team}', Imagefile: '{self.imagefile}')"
+
+class Twok(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    seconds = db.Column(db.Float, nullable=False)
+    date_uploaded = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__():
+        return f"2k(seconds: '{self.seconds}', date_uploaded: '{self.date_uploaded}', user_id: '{self.user_id}')"
+        
 
 @app.route("/")
 def index():
@@ -25,4 +55,5 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
