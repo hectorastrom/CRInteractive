0d3e16d6b13d302b75d0aee3f4bdc50160e5fda5
hectorastrom <hector.astrom@gmail.com>
2021-09-03 23:53:43 -0400

    Add user to database on register

--- app/models.py
+++ app/models.py
@@ -7,14 +7,14 @@ class User(db.Model):
     firstname = db.Column(db.String(30), nullable=False)
     lastname = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    team = db.Column(db.String(20), nullable=False)
-    side = db.Column(db.String(10), nullable=False)
+    team = db.Column(db.String(20), nullable=False, default='none')
+    side = db.Column(db.String(10), nullable=False, default='none')
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     twoks = db.relationship('Twok', backref='rower', lazy=True)
 
     def __repr__(self):
-        return f"User(Firstname: '{self.firstname}', Lastname: '{self.lastname}', Email: '{self.email}', Team: '{self.team}', Imagefile: '{self.imagefile}')"
+        return f"User(Firstname: '{self.firstname}', Lastname: '{self.lastname}', Email: '{self.email}', Team: '{self.team}', Side: {self.side}, Imagefile: '{self.image_file}')"
 
 class Twok(db.Model):
     id = db.Column(db.Integer, primary_key=True)
