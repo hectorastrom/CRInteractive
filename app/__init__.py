d93bd59a75bd7a673820ed11014df23712c201fe
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2021-09-05 00:05:29 -0400

    Added flask migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
+from flask_migrate import Migrate 
 
 
 app = Flask(__name__)
@@ -15,6 +16,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# flask db init
+# flask db migrate -m 'Initial migration'
+# flask db upgrade 
+
+migrate = Migrate(app, db)
+
 bcrypt = Bcrypt()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
