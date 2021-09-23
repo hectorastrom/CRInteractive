15e17837c06f0699e213115e94bc3757bc7b7434
hectorastrom <hector.astrom@gmail.com>
2021-09-22 20:35:00 -0400

    Added scripts.js file

---
+++ app/static/scripts.js
@@ -0,0 +1,9 @@
+// Enable Tooltips
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+// Fade Flashes
+setTimeout(function() {
+    $('.alert').fadeOut('slow');
+}, 2000);
+
