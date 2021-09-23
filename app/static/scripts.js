e8e5699f42a982698cdf2d50fb5bae30f6006e49
hectorastrom <hector.astrom@gmail.com>
2021-09-22 21:43:26 -0400

    Added boilerplate for draw bell curve function

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -7,3 +7,21 @@ setTimeout(function() {
     $('.alert').fadeOut('slow');
 }, 2000);
 
+
+function drawBellCurve(metric_name, metric_value)
+{
+    let header = `
+    <h1>This is a multi-line string which will be written to the html file. We can have "quotes" in here and we can have ${metric_value} variables in here!</h1>
+    `
+    document.write(header)
+}
+
+function enableSorting()
+{
+    $("#sortable").sortable();
+    $("#sortable").disableSelection()
+    
+    $(".box").each(function(index) {
+        console.log(index + ": " + $( this).text())
+    })
+}
