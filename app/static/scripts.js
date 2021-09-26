d77c359f8e2578fcc8d9d2b70e6163c498c28f3a
hectorastrom <hector.astrom@gmail.com>
2021-09-26 11:15:46 -0400

    Added eyeball to toggle view

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -79,10 +79,10 @@ function drawUserBellCurve(metric_tag, metric_name, metric_desc, metric_value, m
 }
 
 
-function drawCoachBellCurve(metric_tag, metric_name, metric_desc, metric_value, metric_importance, button_class, is_checked, firstname)
+function drawCoachBellCurve(metric_tag, metric_name, metric_desc, metric_value, metric_importance, button_class, active, firstname)
 {
     let eyeball = ""
-    if (is_checked)
+    if (active)
     {
         eyeball=`<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
         width="30" height="30"
@@ -99,10 +99,14 @@ function drawCoachBellCurve(metric_tag, metric_name, metric_desc, metric_value, 
         <div class="modal-dialog modal-xl" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title">${metric_name} Curve for ${firstname}</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
+                <h5 class="modal-title">${metric_name} Curve for ${firstname}</h5>
+                <svg id="${metric_tag}_allowEye" class="allow-eye ${active}"
+                xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                width="30" height="30"
+                viewBox="0 0 30 30"><path d="M 15 5 C 6.081703 5 0.32098813 14.21118 0.21679688 14.378906 A 1 1 0 0 0 0 15 A 1 1 0 0 0 0.16210938 15.544922 A 1 1 0 0 0 0.16601562 15.550781 C 0.18320928 15.586261 5.0188313 25 15 25 C 24.938822 25 29.767326 15.678741 29.826172 15.564453 A 1 1 0 0 0 29.837891 15.544922 A 1 1 0 0 0 30 15 A 1 1 0 0 0 29.785156 14.380859 A 1 1 0 0 0 29.783203 14.378906 C 29.679012 14.21118 23.918297 5 15 5 z M 15 8 C 18.866 8 22 11.134 22 15 C 22 18.866 18.866 22 15 22 C 11.134 22 8 18.866 8 15 C 8 11.134 11.134 8 15 8 z M 15 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 18 A 3 3 0 0 0 18 15 A 3 3 0 0 0 15 12 z"></path></svg>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
                 <h5><strong>Description:</strong> ${metric_desc}</h5>
@@ -136,10 +140,7 @@ function drawCoachBellCurve(metric_tag, metric_name, metric_desc, metric_value, 
                         </div>
                     </div>
                     <div class="form-check mt-4">
-                        <div class="row">
-                            <label class = "form-check-label" for="${metric_tag}_view_allowed">Allow ${firstname} to view this chart: </label>
-                            <input type="checkbox" id="${metric_tag}_view_allowed" name="${metric_tag}_view_allowed" class="form-check-input-xl" ${is_checked}>
-                        </div>
+                        <input type="hidden" value="${active}" id="${metric_tag}_view_allowed" name="${metric_tag}_view_allowed" />
                         <div class="form-group short mt-3">
                             <div class="slidecontainer">
                                 <input type="range" min="0" max="10" step="1" value="${metric_importance}" class="slider slider-yellow" name="${metric_tag}_coach_importance">
@@ -176,6 +177,21 @@ function drawCoachBellCurve(metric_tag, metric_name, metric_desc, metric_value, 
             portion = this.value/100.0;
             coordinates = curve.getPointAtLength(portion * totalLength);
             dot.setAttribute("transform", `translate(${coordinates.x}, ${coordinates.y})`);
+        }
+    }
+    let hidden_view_input = document.getElementById(`${metric_tag}_view_allowed`);
+    document.getElementById(`${metric_tag}_allowEye`).onclick = function() {
+        if (this.classList.contains("active"))
+        {
+            this.classList.remove("active");
+            this.style.fill = "rgba(0, 0, 0, .3)";
+            hidden_view_input.value = "";
+        }
+        else
+        {
+            this.classList.add("active");
+            this.style.fill = "rgba(0, 0, 0, .7)";
+            hidden_view_input.value = "active";
         }
     }
 
