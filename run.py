6ebbd7737c67ee5fb19d2a78992ad580ec04eb6f
hectorastrom <hector.astrom@gmail.com>
2021-10-12 21:13:31 -0400

    Commented out db.create_all()

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import app, db, is_production
 
 if __name__ == '__main__':
-    db.create_all()
+    # Said to comment out since alembic couldn't notice this was being done.
+    #db.create_all()
     if is_production:
         app.run(debug=False)
     else:
