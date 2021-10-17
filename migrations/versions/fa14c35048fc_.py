fd1b1db8a2aa81e7ad9e1be7d476ca57f5b4d5e8
hectorastrom <hector.astrom@gmail.com>
2021-10-17 11:15:31 -0400

    generated missing revision

---
+++ migrations/versions/fa14c35048fc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fa14c35048fc
+Revises: 6cb59f1520f5
+Create Date: 2021-10-17 11:15:21.928140
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa14c35048fc'
+down_revision = '6cb59f1520f5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
