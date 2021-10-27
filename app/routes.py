1783cb3d8efae122d6eb0e9dfb01ee5f8ac02e31
hectorastrom <hector.astrom@gmail.com>
2021-10-26 21:38:47 -0400

    Made inputs to drawcurves multiline strings

--- app/routes.py
+++ app/routes.py
@@ -204,7 +204,7 @@ def profile(firstname, id):
 
                 updated_metric.coach_rating = request.form.get(f"{updated_metric.tag}_coach_rating")
                 updated_metric.coach_importance = request.form.get(f"{updated_metric.tag}_coach_importance")
-                updated_metric.note = request.form.get(f"{updated_metric.tag}_coach_notes")
+                updated_metric.note = request.form.get(f"{updated_metric.tag}_coach_notes").strip()
                 updated_metric.view_allowed = bool(request.form.get(f"{updated_metric.tag}_view_allowed"))
                 updated_metric.has_set = True
                 # When there's an update available, a user should have to indicate their rating
